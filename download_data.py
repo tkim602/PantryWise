cb69bd3b8971fa1eeb926e17e97cb088d32b12c0
TaeHo Kim <thkim210414@gmail.com>
2026-08-20 20:33:43 +0900

    docs: publish PantryWise report on gh-pages

--- download_data.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("pes12017000148/food-ingredients-and-recipe-dataset-with-images")
-
-print("Path to dataset files:", path)
